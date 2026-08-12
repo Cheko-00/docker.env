256dd1b2e4f854b1c82eb3bbfacdcd428b22ba6e
Sergio García <sergio.garcia@tikendo.com.mx>
2026-08-12 15:01:48 -0600

    scripts

---
+++ services/init-scripts/01-create-databases.sql
@@ -0,0 +1,6 @@
+-- Script de inicialización para crear las bases de datos requeridas por las aplicaciones
+SELECT 'CREATE DATABASE tickets'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tickets')\gexec
+
+SELECT 'CREATE DATABASE reports'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'reports')\gexec
